b8cb44a50a481d1211da236c4def448fd7854107
ashleyriot000 <ashleyriot000@gmail.com>
2022-03-16 15:40:52 +0900

    예제 생성

---
+++ Assets/BrokenKoreanExample.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenKoreanExample : MonoBehaviour
+{
+    //start, 스타트
+    void Start()
+    {
+        Debug.Log("스타트");
+    }
+
+    // Update 업데이트
+    void Update()
+    {
+        Debug.Log("업데이트");
+    }
+}
