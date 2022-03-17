3c0cff5428dc47242c54b5d3c9337777389e776b
ashleyriot000 <ashleyriot000@gmail.com>
2022-03-17 22:21:57 +0900

    MacOS에서 작성

---
+++ Assets/WriteMac.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriteMac : MonoBehaviour
+{
+
+  // Start 스타트
+  void Start()
+  {
+    Debug.Log("스타트");
+    
+  }
+
+  // Update 업데이트 
+  void Update()
+  {
+    Debug.Log("업데이트");
+  }
+}
